005d4ea5f64de49cf33633f1f4c6db83e836c9fc
Kshitij Rai <kshitijrai0502@gmail.com>
2024-11-20 23:07:20 +0530

    Add files via upload

---
+++ basics1/Hello.java
@@ -0,0 +1,16 @@
+package basics1;
+
+public class Hello { // this is a block
+    public static void main(String[] args) {
+        int age = 26;
+        int aGe = 12;
+        int marks = 98;
+
+        int secondMarksOfStudent = 23; //camelcase
+        int second_allowed$allowed = 12;
+
+        int $marks = 12;
+        int staticWord = 12;
+
+    }
+}
